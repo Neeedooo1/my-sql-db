f3b06f2e16fa1db22cb84d0848f0998da0254556
Neeedooo1 <132912255+Neeedooo1@users.noreply.github.com>
2025-06-15 21:27:51 +0300

    Create students.sql

---
+++ students.sql
@@ -0,0 +1,10 @@
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER
+);
+
+INSERT INTO students (id, name, age) VALUES
+(1, 'Ahmed', 20),
+(2, 'Fatima', 17),
+(3, 'Khalid', 22);
